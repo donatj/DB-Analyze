002ddecd9d5f4ae938e8445e4942c88102b78b47
Jesse Donat <j.donat@capstonedigital.com>
2013-03-06 11:20:00 -0600

    Phar path fixed, fixing chmod

--- create-phar.php
+++ create-phar.php
@@ -5,7 +5,7 @@ $path = dirname(__FILE__) . '/';
 $srcRoot = $path . "src";
 $buildRoot = $path . "build";
 
-$fpath = $buildRoot . "myapp.phar";
+$fpath = $buildRoot . "/myapp.phar";
  
 $phar = new Phar($fpath, 
 	FilesystemIterator::CURRENT_AS_FILEINFO |     	FilesystemIterator::KEY_AS_FILENAME, "myapp.phar");
@@ -14,6 +14,6 @@ $phar->buildFromDirectory($srcRoot,'/\.php$/');
 
 $phar->setStub(file_get_contents($path . 'stub.php'));
 
-//echo (int)chmod($fpath, 0777);
+chmod($fpath, 0777);
 
 echo "Done!\007" . PHP_EOL;
